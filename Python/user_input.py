2f5f999fb1db71b1f5403eb02703f814defacc51
Method Mkoma <gershommethod02@gmail.com>
2024-02-24 00:53:33 +0300

    Intro to Python Assignment

---
+++ Python/user_input.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+loc = input("Enter your location: ")
+print("Hello", name, "you are", age, "years old and live in", loc)
