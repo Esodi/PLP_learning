70d215a7af82fcb5e37ede2afa5a9521286cd755
Method Mkoma <gershommethod02@gmail.com>
2024-03-06 12:28:30 +0300

    Task 2

---
+++ Dart_Programming/week2/task2.dart
@@ -0,0 +1,11 @@
+num subtractTwo(num a, num b)
+{
+  return a - b;
+}
+void main()
+{
+  int a = 76;
+  double b = 54.36;
+  num difference = subtractTwo(a, b);
+  print("The difference btn $a and $b is $difference");
+}
