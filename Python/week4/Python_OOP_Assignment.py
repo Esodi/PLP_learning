2af47654b49dae68fe5ba9931a6901511f7d9d97
Method Mkoma <gershommethod02@gmail.com>
2024-03-14 20:36:38 +0300

    Python OOP Assignment

--- Python/week4/Python_OOP_Assignment.py
+++ Python/week4/Python_OOP_Assignment.py
@@ -1,5 +1,18 @@
 #!/usr/bin/python3
 
+"""
+        Create a Python class named Person.
+    The Person class should have the following attributes:
+
+    name: representing the person's name.
+    age: representing the person's age.
+    gender: representing the person's gender.
+
+    Implement a method called introduce that prints a message introducing the person with their name, age, and gender.
+    Create an instance of the Person class and call the introduce method to display the person's information.
+
+"""
+
 class Person:
     def __init__(self, name, age, gender):
         self.name = name
